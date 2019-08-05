156e970b18b279523621af9f52f103b4fd030375
Pocky <pocky777@nts-corp.com>
2019-08-05 21:39:40 +0900

    수정1

--- review/Pocky/task1/main.js
+++ review/Pocky/task1/main.js
@@ -1,29 +1,42 @@
-var i = 0;
-var elements = document.querySelector('.spinbox').childNodes;
-var spinbox = new Initializer(elements);
 
-function Initializer(ele) {
-    for (i = 0; i < ele.length; i += 1) {
-        if (ele[i].dataset) {
-          if (ele[i].dataset.buttonType === 'input') {
-            this.inputBox = elements[i];
-          }
-          if (ele[i].dataset.buttonType === 'up') {
-            this.upButton = elements[i];
-          }
-          if (ele[i].dataset.buttonType === 'down') {
-            this.downButton = elements[i];
-          }
+var spinbox = {
+  inputBox: null,
+  upButton: null,
+  downButton: null,
+  initializer: function() {
+    var elements = document.querySelector('.spinbox').childNodes;
+    console.log(this);
+    for (var i = 0; i < elements.length; i += 1) {
+      if (elements[i].dataset) {
+        if (elements[i].dataset.buttonType === 'input') {
+          this.inputBox = elements[i];
+          // console.log(elements[i]);
+          // console.log(this.inputBox);
+        }
+        else if (elements[i].dataset.buttonType === 'up') {
+          this.upButton = elements[i];
+          // console.log(elements[i]);
+          // console.log(this.upButton);
+        }
+        else if (elements[i].dataset.buttonType === 'down') {
+          this.downButton = elements[i];
+          // console.log(elements[i]);
+          // console.log(this.downButton);
         }
       }
-}
-function addNum (){
-    var curNum = Number(spinbox.inputBox.value);
+      // console.log(elements[i]);
+    }
+  },
+  addNum: function() {
+    // console.log(this.inputBox);
+    var curNum = Number(this.inputBox.value);
     curNum += 1;
-    spinbox.inputBox.value = curNum;
-}
-function minusNum (){
-    var curNum = Number(spinbox.inputBox.value);
+    this.inputBox.value = curNum;
+  },
+  minusNum: function() {
+    var curNum = Number(this.inputBox.value);
     curNum -= 1;
-    spinbox.inputBox.value = curNum;
+    this.inputBox.value = curNum;
+  }
 }
+spinbox.initializer();

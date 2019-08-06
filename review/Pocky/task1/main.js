fdcb62d3a7d8205205bb17e65252168fc657dd1c
Pocky <user@userui-MacBook-Pro.local>
2019-08-07 01:09:27 +0900

    연속입력 추가

--- review/Pocky/task1/main.js
+++ review/Pocky/task1/main.js
@@ -3,6 +3,7 @@ var spinbox = {
     inputBox: null,
     upButton: null,
     downButton: null,
+    isPressed: false,
     initializer: function () {
         var elements = document.querySelector('.spinbox').childNodes;
         console.log(this);
@@ -10,26 +11,72 @@ var spinbox = {
             if (elements[i].dataset) {
                 if (elements[i].dataset.buttonType === 'input') {
                     this.inputBox = elements[i];
+                    console.log("init input");
                 }
                 else if (elements[i].dataset.buttonType === 'up') {
                     this.upButton = elements[i];
+                    // this.upButton.addEventListener('click', function(e){
+                    //     this.addNum;
+                    // });
+                    console.log("init up");
                 }
                 else if (elements[i].dataset.buttonType === 'down') {
                     this.downButton = elements[i];
+                    // this.downButton.addEventListener('click', function(e){
+                    //     this.minusNum;
+                    // });
+                    console.log("init down");
                 }
             }
         }
     },
+    startPress: function(){
+        console.log("startPress");
+        this.isPressed = true;
+    },
+    endPress: function(){
+        console.log("endPress");
+        this.isPressed = false;
+    },
     addNum: function () {
-        console.log("g");
         var curNum = Number(this.inputBox.value);
         curNum += 1;
         this.inputBox.value = curNum;
+
+        window.setTimeout(function(){
+            // console.log("setTimeout");
+            var timer = window.setInterval(function(){
+                // console.log(this);
+                // console.log("setInterval", spinbox.isPressed);
+                if(spinbox.isPressed){
+                    curNum = Number(spinbox.inputBox.value);
+                    curNum += 1;
+                    spinbox.inputBox.value = curNum;
+                }
+                else
+                    window.clearInterval(timer);
+            },50);
+        },1000);
     },
     minusNum: function () {
         var curNum = Number(this.inputBox.value);
         curNum -= 1;
         this.inputBox.value = curNum;
+
+        window.setTimeout(function(){
+            // console.log("setTimeout");
+            var timer = window.setInterval(function(){
+                // console.log(this);
+                // console.log("setInterval", spinbox.isPressed);
+                if(spinbox.isPressed){
+                    curNum = Number(spinbox.inputBox.value);
+                    curNum -= 1;
+                    spinbox.inputBox.value = curNum;
+                }
+                else
+                    window.clearInterval(timer);
+            },50);
+        },500);
     }
 }
 spinbox.initializer();

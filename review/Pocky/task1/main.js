8adfd660b1e012e8b3111dabe6b59e8d27e6c66d
Pocky <pocky777@nts-corp.com>
2019-08-06 21:25:14 +0900

    탭사이즈

--- review/Pocky/task1/main.js
+++ review/Pocky/task1/main.js
@@ -1,42 +1,35 @@
 
 var spinbox = {
-  inputBox: null,
-  upButton: null,
-  downButton: null,
-  initializer: function() {
-    var elements = document.querySelector('.spinbox').childNodes;
-    console.log(this);
-    for (var i = 0; i < elements.length; i += 1) {
-      if (elements[i].dataset) {
-        if (elements[i].dataset.buttonType === 'input') {
-          this.inputBox = elements[i];
-          // console.log(elements[i]);
-          // console.log(this.inputBox);
+    inputBox: null,
+    upButton: null,
+    downButton: null,
+    initializer: function () {
+        var elements = document.querySelector('.spinbox').childNodes;
+        console.log(this);
+        for (var i = 0; i < elements.length; i += 1) {
+            if (elements[i].dataset) {
+                if (elements[i].dataset.buttonType === 'input') {
+                    this.inputBox = elements[i];
+                }
+                else if (elements[i].dataset.buttonType === 'up') {
+                    this.upButton = elements[i];
+                }
+                else if (elements[i].dataset.buttonType === 'down') {
+                    this.downButton = elements[i];
+                }
+            }
         }
-        else if (elements[i].dataset.buttonType === 'up') {
-          this.upButton = elements[i];
-          // console.log(elements[i]);
-          // console.log(this.upButton);
-        }
-        else if (elements[i].dataset.buttonType === 'down') {
-          this.downButton = elements[i];
-          // console.log(elements[i]);
-          // console.log(this.downButton);
-        }
-      }
-      // console.log(elements[i]);
+    },
+    addNum: function () {
+        console.log("g");
+        var curNum = Number(this.inputBox.value);
+        curNum += 1;
+        this.inputBox.value = curNum;
+    },
+    minusNum: function () {
+        var curNum = Number(this.inputBox.value);
+        curNum -= 1;
+        this.inputBox.value = curNum;
     }
-  },
-  addNum: function() {
-    // console.log(this.inputBox);
-    var curNum = Number(this.inputBox.value);
-    curNum += 1;
-    this.inputBox.value = curNum;
-  },
-  minusNum: function() {
-    var curNum = Number(this.inputBox.value);
-    curNum -= 1;
-    this.inputBox.value = curNum;
-  }
 }
 spinbox.initializer();

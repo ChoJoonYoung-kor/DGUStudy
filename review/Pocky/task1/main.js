30d76a1184e4994c2f0888ee0f35561029ff3fdd
Pocky <pocky777@nts-corp.com>
2019-08-07 18:02:53 +0900

    객체 함수로 수정

--- review/Pocky/task1/main.js
+++ review/Pocky/task1/main.js
@@ -1,82 +1,94 @@
 
-var spinbox = {
-    inputBox: null,
-    upButton: null,
-    downButton: null,
-    isPressed: false,
-    initializer: function () {
-        var elements = document.querySelector('.spinbox').childNodes;
-        console.log(this);
+var ele_spinbox = document.querySelectorAll('.spinbox');
+var obj_spinbox = [];
+function spinbox (){
+    this.inputBox = null;
+    this.upButton = null;
+    this.downButton = null;
+    this.isPressed = false;
+    this.initializer = function (ele_box) {
+        var elements = ele_box.childNodes;
         for (var i = 0; i < elements.length; i += 1) {
             if (elements[i].dataset) {
                 if (elements[i].dataset.buttonType === 'input') {
                     this.inputBox = elements[i];
-                    console.log("init input");
+                    // console.log("init input");
                 }
                 else if (elements[i].dataset.buttonType === 'up') {
                     this.upButton = elements[i];
-                    // this.upButton.addEventListener('click', function(e){
-                    //     this.addNum;
-                    // });
-                    console.log("init up");
+                    this.upButton.addEventListener('click', this.addNum.bind(this));
+                    this.upButton.addEventListener('mousedown', this.addNum.bind(this));
+                    this.upButton.addEventListener('mouseup', this.addNum.bind(this));
+                    this.upButton.addEventListener('mousedown', this.checkPress.bind(this));
+                    this.upButton.addEventListener('mouseup', this.checkPress.bind(this));
+                    // console.log("init up");
                 }
                 else if (elements[i].dataset.buttonType === 'down') {
                     this.downButton = elements[i];
-                    // this.downButton.addEventListener('click', function(e){
-                    //     this.minusNum;
-                    // });
-                    console.log("init down");
+                    this.downButton.addEventListener('click', this.minusNum.bind(this));
+                    this.downButton.addEventListener('mousedown', this.minusNum.bind(this));
+                    this.downButton.addEventListener('mouseup', this.minusNum.bind(this));
+                    this.downButton.addEventListener('mousedown', this.checkPress.bind(this));
+                    this.downButton.addEventListener('mouseup', this.checkPress.bind(this));
+                    // console.log("init down");
                 }
             }
         }
-    },
-    startPress: function(){
-        console.log("startPress");
-        this.isPressed = true;
-    },
-    endPress: function(){
-        console.log("endPress");
-        this.isPressed = false;
-    },
-    addNum: function () {
+    }
+    this.checkPress = function(event){
+        // console.log("startPress", event.type);
+        if(event.type === "mousedown")
+            this.isPressed = true;
+        else
+            this.isPressed = false;
+    }
+    this.addNum = function (event) {
+        // console.log("addNum");
         var curNum = Number(this.inputBox.value);
-        curNum += 1;
-        this.inputBox.value = curNum;
-
-        window.setTimeout(function(){
-            // console.log("setTimeout");
-            var timer = window.setInterval(function(){
-                // console.log(this);
-                // console.log("setInterval", spinbox.isPressed);
-                if(spinbox.isPressed){
-                    curNum = Number(spinbox.inputBox.value);
-                    curNum += 1;
-                    spinbox.inputBox.value = curNum;
-                }
-                else
-                    window.clearInterval(timer);
-            },50);
-        },1000);
-    },
-    minusNum: function () {
+        if(event.type === "click"){
+            curNum += 1;
+            this.inputBox.value = curNum;
+        }
+        else{
+            window.setTimeout(function () {
+                var timer = window.setInterval(function () {
+                    // console.log(this.isPressed);
+                    if (this.isPressed) {
+                        curNum = Number(this.inputBox.value);
+                        curNum += 1;
+                        this.inputBox.value = curNum;
+                    }
+                    else
+                        window.clearInterval(timer);
+                }.bind(this), 50);
+            }.bind(this), 1000);
+        }
+    }
+    this.minusNum = function (event) {
+        // console.log("minusNum");
         var curNum = Number(this.inputBox.value);
-        curNum -= 1;
-        this.inputBox.value = curNum;
-
-        window.setTimeout(function(){
-            // console.log("setTimeout");
-            var timer = window.setInterval(function(){
-                // console.log(this);
-                // console.log("setInterval", spinbox.isPressed);
-                if(spinbox.isPressed){
-                    curNum = Number(spinbox.inputBox.value);
-                    curNum -= 1;
-                    spinbox.inputBox.value = curNum;
-                }
-                else
-                    window.clearInterval(timer);
-            },50);
-        },500);
+        if (event.type === "click") {
+            curNum -= 1;
+            this.inputBox.value = curNum;
+        }
+        else{
+            window.setTimeout(function () {
+                var timer = window.setInterval(function () {
+                    // console.log(this.isPressed);
+                    if (this.isPressed) {
+                        curNum = Number(this.inputBox.value);
+                        curNum -= 1;
+                        this.inputBox.value = curNum;
+                    }
+                    else
+                        window.clearInterval(timer);
+                }.bind(this), 50);
+            }.bind(this), 500);
+        }
     }
 }
-spinbox.initializer();
+for (var i = 0; i < ele_spinbox.length; i += 1) {
+    var newobj = new spinbox(ele_spinbox[i]);
+    newobj.initializer(ele_spinbox[i]);
+    obj_spinbox.push(newobj);
+}

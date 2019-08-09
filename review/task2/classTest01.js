dc0eb79f5dcb696dcb78a8c58028282acd97b495
chaneee <rucold@naver.com>
2019-08-10 02:09:10 +0900

    깃 손에 익을때까지

--- review/task2/classTest01.js
+++ review/task2/classTest01.js
@@ -1,4 +1,4 @@
-//방법1(과제에서 원하는 방식?)(aka.깔끔)
+/*방법1(과제에서 원하는 방식?)(aka.깔끔)*/
 function student (info){
 
     this.getName = function(){
@@ -17,9 +17,9 @@ function student (info){
 var stu1 = new student({id: 1004, name: '조준영', scoreEnglish: 80, scoreMath: 50});
 alert(stu1.toString());
 alert(stu1.getName() + '의 평균점수: ' + stu1.getAverage());
+///////
 
-
-//방법2(이방법은 외않되?)
+/*방법2(이방법은 외않되?)*/
 function student2(){
     this.id = 0;
     this.name = "";
@@ -49,9 +49,9 @@ alert(stu2.toString());
     ///중간에 원하는 값만 쉽게 변경 가능한데
 stu2.name = "꼬르꼬투"
 alert(stu2.getName() + '의 평균점수: ' + stu2.getAverage());
+///////
 
-
-//방법3(객체 리터럴) 찾다가 이런방법도 있어서 해봄
+/*방법3(객체 리터럴) 찾다가 이런방법도 있어서 해봄*/
 //싱글톤이라 하나밖에 생성 못해서 클래스는 아니긴한듯
 var student3 = {
     id : 0,
